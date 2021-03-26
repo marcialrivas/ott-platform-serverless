addf592169926dbed69b0f349d61a5e0e4d5c00d
EC2 Default User <ec2-user@ip-10-0-0-26.ec2.internal>
2021-03-26 23:11:20 +0000

    # feat: Stage deployment stage pipeline * feat: Add serverless deploy stesp * bug: Update lamda_get_movie handler impots * feat: create serverless.yml file

--- front-end-microservices/movies/lambda_get_movie.py
+++ front-end-microservices/movies/lambda_get_movie.py
@@ -1,7 +1,7 @@
 __author__ = 'mrivas'
 
-from main.lambda_get_movie_handler import lambda_get_movie_handler
+from main.lambda_get_movie_handler import lambda_handler
 
 def handler(event, context):
-    hdl = lambda_get_movie_handler()
+    hdl = lambda_handler()
     return hdl.handle(event, context)
